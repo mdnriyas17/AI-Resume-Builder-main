5e5b49869c85a3e927f032f75e515ba769cc0150
Mohamedriyas <123050293+mdnriyas17@users.noreply.github.com>
2024-10-10 14:56:09 +0530

    Update GlobalApi.js

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_APP_BASE_URL+"/api",
+  baseURL: https://ai-resume-frik.onrender.com+"/api",
   headers: {
     "Content-Type": "application/json",
   },
